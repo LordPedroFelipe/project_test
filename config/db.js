cc4227edab348052a9695e546e3484fae74e2e14
DevPedroFelipe <pf.souza15@gmail.com>
2019-10-06 10:08:32 -0300

    Revert "Pedro Felipe - Commit inicial do projeto"
    This reverts commit 129eb745aa1fa08c7436fe216eda71cb085f4f28.

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-module.exports = mysql.createConnection({
-    host : '35.193.118.123',
-    port : '3306',
-    user : 'challenge',
-    password : '#Ch4L13ngE@Pwd#',
-    database : 'ops_challenge'
-});
